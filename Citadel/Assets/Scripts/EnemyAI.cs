71d7333e87e7ee64395d5c7c85a8eb718d228e51
Hughtee <hughtee14@gmail.com>
2015-01-29 01:04:56 -0800

    Started AI Script

---
+++ Citadel/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+
+	private Vector3 Player;
+	private Vector2 Playerdirection;
+	private float XDif;
+	private float YDif;
+	
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+		Player = GameObject.Find ("Player").transform;
+
+
+		XDif = Player.x - transform.positionX;
+		yDif = Player.Y - transform.positionY;
+	
+		Playerdirection = new Vector2 (XDiff, YDiff);
+		
+
+			rigibody2D.AddForce (Playerdirection.normalized * speed);
+	}
+}	
