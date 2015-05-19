769b065c128f2c69b05a7c2eb8836126a926f8b3
Hughtee <hughtee14@gmail.com>
2015-05-18 18:24:37 -0700

    Added Instruction Screen.

--- Citadel/Assets/Scripts/PauseMenu.cs
+++ Citadel/Assets/Scripts/PauseMenu.cs
@@ -4,46 +4,7 @@ using System.Collections;
 public class PauseMenu : MonoBehaviour {
 
 
-	/*bool paused = false;
-	
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Escape))
-			paused = togglePause();
-	}
-	
-	void OnGUI()
-	{
-		if (paused) 
-		{
-			GUILayout.Label ("Game is paused!");
-			if (GUILayout.Button ("Click me to unpause"))
-			paused = togglePause ();
-		} 
 
-		else 
-		{
-			GUILayout.Label("Game is unpaused!");
-			if(GUILayout.Button("Click me to pause"))
-				paused = togglePause ();
-		
-		}
-
-	}
-	
-	bool togglePause()
-	{
-		if(Time.timeScale == 0f)
-		{
-			Time.timeScale = 1f;
-			return(false);
-		}
-		else
-		{
-			Time.timeScale = 0f;
-			return(true);    
-		}
-	} */
 	//refrence for the pause menu panel in the hierarchy
 	public GameObject pauseMenuPanel;
 	//animator reference
---
+++ Citadel/Assets/Scripts/instruction.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class instruction : MonoBehaviour {
+
+	//refrence for the pause menu panel in the hierarchy
+	public GameObject instructionPanel;
+	//animator reference
+	private Animator anim;
+	//variable for checking if the game is paused 
+	private bool isInstructed = false;
+	// Use this for initialization
+	
+	
+	void Start () {
+
+		
+		//get the animator component
+		anim = instructionPanel.GetComponent<Animator>();
+		//disable it on start to stop it from playing the default animation
+		//anim.enabled = false;
+	}
+	
+	// Update is called once per frame
+	
+	
+	public void Update () 
+	{
+		//pause the game on escape key press and when the game is not already paused
+		if(Input.GetKeyUp(KeyCode.Escape) && !isInstructed)
+		{
+			Instructed();
+		}
+		//unpause the game if its paused and the escape key is pressed
+		else if(Input.GetKeyUp(KeyCode.Escape) && isInstructed)
+		{
+			UnInstructed();
+		}
+	}
+	
+	//function to pause the game
+	
+	
+	public void Instructed(){
+		//enable the animator component
+		anim.enabled = true;
+		//play the Slidein animation
+		anim.Play("InstructionIn");
+		//set the isPaused flag to true to indicate that the game is paused
+		isInstructed = true;
+		
+	}
+	//function to unpause the game
+	
+	
+	public void UnInstructed(){
+		//set the isPaused flag to false to indicate that the game is not paused
+		isInstructed = false;
+		//play the SlideOut animation
+		anim.Play("InstructionOut");
+		//set back the time scale to normal time scale
+		
+	}
+
+}
